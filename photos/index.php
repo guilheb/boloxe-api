3e36194f37baec8e3b8cf47d353a048ec2adf60f
Guillaume Hébert <guillaume@cybercat.ca>
2024-02-16 12:51:37 -0400

    Ne rien filtrer si HTTP_ORIGIN n'est pas setté

--- photos/index.php
+++ photos/index.php
@@ -7,15 +7,17 @@ require_once 'FlickrAPI.php';
 require_once 'Entities/Album.php';
 require_once 'Entities/Photo.php';
 
-// Allow localhost or boloxe.com
-$regex = '/^http(s)?:\/\/((.*\.)?localhost|(www.)?boloxe.com)(:\d+)?/';
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Allow localhost or boloxe.com
+    $regex = '/^http(s)?:\/\/((.*\.)?localhost|(www.)?boloxe.com)(:\d+)?/';
 
-if (preg_match($regex, $_SERVER['HTTP_ORIGIN'] ?? null, $matches)) {
-    header("Access-Control-Allow-Origin: {$matches[0]}");
-    header("Vary: Origin");
-} else {
-    http_response_code(403);
-    exit;
+    if (preg_match($regex, $_SERVER['HTTP_ORIGIN'] ?? null, $matches)) {
+        header("Access-Control-Allow-Origin: {$matches[0]}");
+        header("Vary: Origin");
+    } else {
+        http_response_code(403);
+        exit;
+    }
 }
 
 $photos = new API\Photos\Photos();
